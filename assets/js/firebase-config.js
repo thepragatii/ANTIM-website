0a7e7e760fb4ddc57a2f9149cfaff27b38daa8c8
thepragatii <pragatikhare214@gmail.com>
2026-06-21 11:32:20 +0530

    Delete firebase-config.js script tag

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,4 +1,4 @@
-<script type="module">
+<script 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
