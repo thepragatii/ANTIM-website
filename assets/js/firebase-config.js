815e7e3d4070d8b77a10f47316c38cd572aea36e
thepragatii <pragatikhare214@gmail.com>
2026-06-21 11:34:43 +0530

    Fix Firebase initialization

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,23 +1,21 @@
-<script 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDkDQ80sVBO3K1NbaLU41D7ebcK8nihlSo",
-    authDomain: "antim-fde23.firebaseapp.com",
-    projectId: "antim-fde23",
-    storageBucket: "antim-fde23.firebasestorage.app",
-    messagingSenderId: "600792086326",
-    appId: "1:600792086326:web:488fcbb0855ccd1907e597",
-    measurementId: "G-XXSFJVKWGL"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDkDQ80sVBO3K1NbaLU41D7ebcK8nihlSo",
+  authDomain: "antim-fde23.firebaseapp.com",
+  projectId: "antim-fde23",
+  storageBucket: "antim-fde23.firebasestorage.app",
+  messagingSenderId: "600792086326",
+  appId: "1:600792086326:web:488fcbb0855ccd1907e597"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
